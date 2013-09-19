b056752e19596f0b44d2a67e3642b22bcfde07bc
H2CHANG <sexbear@gmail.com>
2013-09-20 00:50:36 +0800

    handle captial letter, plural, punctuation

--- IR/test.pl
+++ IR/test.pl
@@ -1,26 +1,38 @@
 #!/usr/bin/perl
 
-# use module
-use XML::Simple;
-use Data::Dumper;
 use strict;
 use warnings;
 
+# use module
+use XML::Simple;
+use Data::Dumper;
+use Lingua::EN::PluralToSingular 'to_singular';
+
+
 our %pubmed_data;
 our %word_data;
+our %true_data;
 # create object
 my $xml = new XML::Simple (KeyAttr=>[]);
 
-my $data = $xml->XMLin("data2.txt");
+my $data = $xml->XMLin("data1.txt");
 my ($key1, $value1, @key_array);
 my $xml_file;
+my $out_file;
 open $xml_file, '>', 'data2.txt'
     or die "can't open $!";
+
+open $out_file, '>', 'data4.txt'
+    or die "can't open $!";
+
 #dereference hash ref
 #
-print $data->{MedlineCitation}->{PMID}->{content}, "\n";
+#print $data->{MedlineCitation}->{PMID}->{content}, "\n";
 
 traverse( $data );
+print "\n------------------------------------------------\n";
+ 
+print to_singular ('S');
 print "\n------------------------------------------------\n";
 
 foreach (sort keys %pubmed_data ){
@@ -45,12 +57,40 @@ foreach (sort keys %pubmed_data ){
     }
 
 foreach (sort keys %word_data ){
-    print $xml_file "key = $_\n";
-    print $xml_file "value = $word_data{$_}\n";
+    my $key = $_;
+    my $value = $word_data{$_};
+    my $sigular_key;
+    #$_ =~ s/(.*),/$1/;
+    #$key =~ s/(.*),/$1/;
+    #$key =~ s/\((.*)\)/$1/;
+    #$key =~ s/(.*)(\.|\?|\:)$/$1/;
+    $key =~ s/[^\w\d\s]//g;
+    $key = lc $key;
+    
+    $sigular_key = to_singular ($key);
+    $sigular_key = $key if ($key eq 's');
+    if (exists $true_data{$sigular_key}{1} ) {
+            $true_data{$sigular_key}{1} += $value;
+            
+            }
+    else {
+             $true_data{$sigular_key}{1} = $value;
+            }
+    print $xml_file "key = $key\n";
+    print $xml_file "value = $value\n";
     
     
    }
+
 close $xml_file;
+foreach (sort keys %true_data) {
+    print $out_file "key = $_\n";
+    print $out_file "value =  $true_data{$_}{1}\n";
+       
+    }
+close $out_file;
+
+
 
 sub traverse {
     our %pubmed_data;
