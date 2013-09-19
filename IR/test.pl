584e5ce0916ee89cedbe811d75259116eb1e0266
H2CHANG <sexbear@gmail.com>
2013-09-19 11:48:44 +0800

    new

--- IR/test.pl
+++ IR/test.pl
@@ -7,11 +7,15 @@ use strict;
 use warnings;
 
 our %pubmed_data;
+our %word_data;
 # create object
 my $xml = new XML::Simple (KeyAttr=>[]);
 
-my 	$data = $xml->XMLin("data.txt");
-
+my $data = $xml->XMLin("data2.txt");
+my ($key1, $value1, @key_array);
+my $xml_file;
+open $xml_file, '>', 'data2.txt'
+    or die "can't open $!";
 #dereference hash ref
 #
 print $data->{MedlineCitation}->{PMID}->{content}, "\n";
@@ -23,16 +27,37 @@ foreach (sort keys %pubmed_data ){
     
     print "key = $_\n";
     print "value = $pubmed_data{$_}\n";
-    
+    $key1 = $_;
+    $key1=~ s/!\s+//;
+    $value1 = $pubmed_data{$_};
+    @key_array = split(/\s+/, $key1);
+    foreach (@key_array) {
+        if (exists $word_data{$_} ) {
+            $word_data{$_}++;
+            
+            }
+        else {
+            $word_data{$_} = $value1;
+            }
+        
+        }
     
     }
+
+foreach (sort keys %word_data ){
+    print $xml_file "key = $_\n";
+    print $xml_file "value = $word_data{$_}\n";
+    
+    
+   }
+close $xml_file;
 
 sub traverse {
     our %pubmed_data;
     my ($element) = @_;
     if( ref( $element ) =~ /HASH/ ) {
         foreach my $key (keys %$element) {
-            print "key=$key\n";
+  #          print "key=$key\n";
             next if ($key eq "PubmedData");
             traverse( $$element{$key} );
             }
@@ -42,7 +67,7 @@ sub traverse {
         traverse( $_ ) foreach @$element;
     } 
     else {
-        print "$element\n";
+   #     print "$element\n";
         if (exists $pubmed_data{$element} ) {
             $pubmed_data{$element}++;
             
